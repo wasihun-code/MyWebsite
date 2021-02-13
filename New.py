81c4fa1b2f53ca8ffe72f1a9b0da751cc5ed48b1
wasihun-code <wasihunageru7947@gmail.com>
2021-02-13 11:52:36 +0300

    first change

---
+++ New.py
@@ -0,0 +1,78 @@
+import math
+"""A simple application to gather personal
+information about kebele residents"""
+"""
+first_Name = input ('What is your First Name? >>>')
+print("    ")
+Middle_Name = input ('What is your Middle Name? >>> ')
+print("     ")
+Last_Name = input ('What is your Last Name?      >>> ')
+print("     ")
+Age = input ('Please provide your age Here       >>> ')
+Place_of_birth = ('Where were you born?          >>> ')
+print("    ")
+
+Captialized_FN = first_Name.capitalize()
+Capitalized_MN = Middle_Name.capitalize()
+Capitalized_LN = Last_Name.capitalize()
+print("***********************************")
+print ( "Name: ", Captialized_FN+' '+Capitalized_MN+' '+Capitalized_LN)
+print ("Age: " , Age)
+print("*************************************")
+print("      ")
+thanks = "Thank you for choosing us."
+print ("    " ,thanks)
+print ("   ")
+"""
+"""
+print("  ")
+print("  ")
+User_input1= input ("Please enter the first number >> ")
+User_input2= input ("Please enter the second number >> ")
+print(" ")
+first_number = int(User_input1)
+second_number = int (User_input2)
+print(" ")
+
+addition = first_number + second_number
+multiplication  = first_number * second_number
+division =  first_number* second_number 
+subtraction =first_number- second_number
+modulus = first_number % second_number
+exponent =  first_number ** second_number
+
+print (" The additon of the numbers is" , addition)
+print (" ")
+print (" The subtraction of the numbers is " , subtraction)
+print (" ")
+print (" The division of the numbers is " , division)
+print (" ")
+print (" The modulus of the numbers is " , modulus)
+print (" ")
+print (" The exponent form of the numbers is  " , exponent)
+"""
+
+"""
+from math import exp
+from math import log
+User_number = input (" please enter the number")
+intered_Number = int(User_number)
+print (" The absoulute value of your number is :" , abs(intered_Number))
+print (" The exponential  value of the number is :" , exp(intered_Number))
+print ("The logartihmic form of the number is :" , log(intered_Number))
+print ("The logartithmic 10 for the no is: " , log10(intered_Number))
+"""
+"""
+abel_height = 23.234
+kebedes_height = 79
+shuluqas_height = 90
+print (" The maximum of the numbers Is:" , max (abel_height, kebedes_height, shuluqas_height))
+print (" The maximum of the numbers Is:" , min (abel_height, kebedes_height, shuluqas_height))
+print( " the exponential form of the height is:" , math.pow(abel_height,kebedes_height))
+print( "When ablel's height is rounded to 2 decimal no is: " , round(abel_height, 2))
+"""
+"""
+import random
+print (" What " , random.choice(range(100)))
+print(" why" , random.randrange(1, 100, 3))
+"""
